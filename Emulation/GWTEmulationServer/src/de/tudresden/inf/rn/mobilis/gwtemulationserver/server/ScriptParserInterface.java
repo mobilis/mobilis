9102382301cbc1169813a699337b38c28a582cf7
ubuntudroid <ubuntudroid@gmail.com>
2012-12-04 17:57:06 +0100

    Optimize emulation script language; generate JAXB classes from XSD; first (incomplete) implementation of XMLScriptParser

--- Emulation/GWTEmulationServer/src/de/tudresden/inf/rn/mobilis/gwtemulationserver/server/ScriptParserInterface.java
+++ Emulation/GWTEmulationServer/src/de/tudresden/inf/rn/mobilis/gwtemulationserver/server/ScriptParserInterface.java
@@ -2,6 +2,8 @@ package de.tudresden.inf.rn.mobilis.gwtemulationserver.server;
 
 import java.io.File;
 
+import de.tudresden.inf.rn.mobilis.gwtemulationserver.server.script.Script;
+
 public interface ScriptParserInterface {
 	
 	public abstract Script parse(File script);
