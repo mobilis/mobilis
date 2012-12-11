73611a246eea33a01b3ed7ceec8f2f45afeca334
ubuntudroid <ubuntudroid@gmail.com>
2012-12-11 18:14:46 +0100

    Add abstract ScriptExecutor class which routes precompiled commands to specific implementation (server/client)

---
+++ Emulation/GWTEmulationServer/src/de/tudresden/inf/rn/mobilis/gwtemulationserver/server/script/notgen/InstanceGroupImpl.java
@@ -0,0 +1,26 @@
+package de.tudresden.inf.rn.mobilis.gwtemulationserver.server.script.notgen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.tudresden.inf.rn.mobilis.gwtemulationserver.server.script.InstanceGroupType;
+import de.tudresden.inf.rn.mobilis.gwtemulationserver.server.script.InstanceType;
+
+public class InstanceGroupImpl extends InstanceGroupType {
+
+	private List<InstanceType> instances = new ArrayList<InstanceType>();
+	
+	public InstanceGroupImpl(InstanceGroupType instanceGroup) {
+		for (int i = instanceGroup.getFirstInstanceId(); i < instanceGroup.getInstanceCount() - instanceGroup.getFirstInstanceId() + 2; i++) {
+			InstanceType instance = new InstanceType();
+			instance.setAppNS(instanceGroup.getAppNS());
+			instance.setInstanceId(i);
+			instances.add(instance);
+		}
+	}
+	
+	public List<InstanceType> getInstances() {
+		return instances;
+	}
+	
+}
