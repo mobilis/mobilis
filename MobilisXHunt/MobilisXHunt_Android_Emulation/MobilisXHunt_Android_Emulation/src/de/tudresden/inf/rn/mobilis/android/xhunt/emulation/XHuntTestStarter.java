912390e7d303cb0cc5628c5aee39e7a1b32608b9
Thomas Walther <thomaswalther85@googlemail.com>
2013-03-28 03:45:41 +0100

    Mobilis XHunt Emulation: - some major changes - stability of script-execution

--- MobilisXHunt/MobilisXHunt_Android_Emulation/MobilisXHunt_Android_Emulation/src/de/tudresden/inf/rn/mobilis/android/xhunt/emulation/XHuntTestStarter.java
+++ MobilisXHunt/MobilisXHunt_Android_Emulation/MobilisXHunt_Android_Emulation/src/de/tudresden/inf/rn/mobilis/android/xhunt/emulation/XHuntTestStarter.java
@@ -7,61 +7,52 @@ import android.test.InstrumentationTestSuite;
 import android.util.Log;
 
 public class XHuntTestStarter extends InstrumentationTestRunner {
-	
-	private static final String TAG = "XHuntAutomatic - TestStarter";
-	
-	private Boolean isMisterX = true;
-	private String userName = "";
-	private Integer playerCount = 0;
-	
+
+	private static final String TAG = "Mobilis XHunt Emulation - TestStarter";
+
+	private String startID = "";
+	private String host = "";
+
 	@Override
 	public void onCreate(Bundle settings) {
-		
+
 		Log.d(TAG, "onCreate im TestStarter");
-		
-		if(settings.containsKey("userName") && settings.containsKey("isMisterX")) {
-			isMisterX = settings.getBoolean("isMisterX");
-			userName = settings.getString("userName");
-			Log.d(TAG, "userName: " + userName);
-			Log.d(TAG, "isMisterX: " + isMisterX);
-		} else {
-			Log.d(TAG, "noSetting");
+
+		if(settings.containsKey("startID")) {
+			startID = settings.getString("startID");
+			Log.d(TAG, startID);
 		}
-		
-		if(settings.containsKey("playerCount")) {
-			playerCount = settings.getInt("playerCount");
+		if(settings.containsKey("host")) {
+			host = settings.getString("host");
+			Log.d(TAG, host);
 		}
-		
+
 		super.onCreate(settings);
-		
+
+	}
+
+	public String getStartID() {
+		return startID;
 	}
 	
-	public Boolean isMisterX() {
-		return isMisterX;
+	public String getHost() {
+		return host;
 	}
-	
-	public String getUserName() {
-		return userName;
-	}
-	
-	public Integer getPlayerCount() {
-		return playerCount;
-	}
-	
+
 	@Override
     public TestSuite getAllTests() {
 		Log.d(TAG, "getAllTests()");
-		
+
         InstrumentationTestSuite suite = new InstrumentationTestSuite(this);
         suite.addTestSuite(MainTest.class);
-        
+
         return suite;
     }
-	
+
 	@Override
     public ClassLoader getLoader() {
 		Log.d(TAG, "getLoader()");
-		
+
         return XHuntTestStarter.class.getClassLoader();
     }
 
---
+++ MobilisXHunt/MobilisXHunt_Android_Emulation/MobilisXHunt_Android_Emulation/src/de/tudresden/inf/rn/mobilis/android/xhunt/emulation/beans/IEmulationOutgoing.java
@@ -0,0 +1,11 @@
+package de.tudresden.inf.rn.mobilis.android.xhunt.emulation.beans;
+
+import de.tudresden.inf.rn.mobilis.xmpp.beans.XMPPBean;
+
+public interface IEmulationOutgoing {
+
+	void sendXMPPBean( XMPPBean out, IXMPPCallback< ? extends XMPPBean > callback );
+
+	void sendXMPPBean( XMPPBean out );
+
+}
