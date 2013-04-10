0c32d54f669f844457efe7f2aa024fa3fd1a3d1b
Matthias Köngeter <s9581863@mailbox.tu-dresden.de>
2013-04-10 20:04:59 +0200

    minor fix

--- MobilisXHunt/MobilisXHunt_Service/src/de/tudresden/inf/rn/mobilis/services/xhunt/state/GameStateGameOver.java
+++ MobilisXHunt/MobilisXHunt_Service/src/de/tudresden/inf/rn/mobilis/services/xhunt/state/GameStateGameOver.java
@@ -19,10 +19,14 @@
  ******************************************************************************/
 package de.tudresden.inf.rn.mobilis.services.xhunt.state;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import de.tudresden.inf.rn.mobilis.services.xhunt.Game;
 import de.tudresden.inf.rn.mobilis.services.xhunt.XHunt;
+import de.tudresden.inf.rn.mobilis.services.xhunt.model.XHuntPlayer;
 import de.tudresden.inf.rn.mobilis.services.xhunt.proxy.GameDetailsRequest;
 import de.tudresden.inf.rn.mobilis.services.xhunt.proxy.GameOverResponse;
 import de.tudresden.inf.rn.mobilis.xmpp.beans.XMPPBean;
@@ -63,7 +67,12 @@ public class GameStateGameOver extends GameState{
 		LOGGER.info("GameStateGameOver#checkShutdownCondition: "
 				+ "shutdown in gameOverState?: " + (game.getPlayers().size() == 0));
 		
-		if(game.getPlayers().size() == 0){
+		Set<XHuntPlayer> avlblPlayers = new HashSet<XHuntPlayer>();
+		for(Map.Entry<String, XHuntPlayer> entry : game.getPlayers().entrySet())
+			if(entry.getValue().isOnline())
+				avlblPlayers.add(entry.getValue());
+		
+		if(avlblPlayers.size() == 0){
 			try {
 				control.shutdown();
 			} catch (Exception e) {
