19f459f4df3324ebfc4a3a1f04e95e02933276b8
ubuntudroid <ubuntudroid@gmail.com>
2013-05-14 13:56:09 +0200

    Remove Emulation code (moved to own repository)

--- Emulation/GWTEmulationServer/generated/client/IEmulationOutgoing.java
+++
@@ -1,9 +0,0 @@
-package de.tudresden.inf.rn.mobilis.emulation.clientstub;
-
-public interface IEmulationOutgoing {
-
-	void sendXMPPBean( XMPPBean out, IXMPPCallback< ? extends XMPPBean > callback );
-
-	void sendXMPPBean( XMPPBean out );
-
-}
