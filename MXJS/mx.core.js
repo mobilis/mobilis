eb5f4aa4e0be2827d30be76b73f9d1cff76b3f17
mcnesium <mcnesium@versteht.es>
2013-10-23 16:31:42 +0200

    removed self-executing anonymous function code

--- MXJS/mx.core.js
+++ MXJS/mx.core.js
@@ -5,169 +5,146 @@
 *  It extends the MX object created in the main file mx.js
 */
 
-(function() {
 
-    var core = {
+var core = {
 
-        init: function() {
-            MX.addNamespace('URL', 'http://mobilis.inf.tu-dresden.de');
-            MX.addNamespace('COORDINATOR', MX.NS.URL + '#services/CoordinatorService');
-        },
+    init: function() {
+        MX.addNamespace('URL', 'http://mobilis.inf.tu-dresden.de');
+        MX.addNamespace('COORDINATOR', MX.NS.URL + '#services/CoordinatorService');
+    },
 
-        Status: {
-            ERROR: 0,
-            CONNECTING: 1,
-            CONNFAIL: 2,
-            AUTHENTICATING: 3,
-            AUTHFAIL: 4,
-            CONNECTED: 5,
-            DISCONNECTED: 6,
-            DISCONNECTING: 7,
-            ATTACHED: 8
-        },
-
+    Status: { // TODO: refactor this
+        ERROR: 0,
+        CONNECTING: 1,
+        CONNFAIL: 2,
+        AUTHENTICATING: 3,
+        AUTHFAIL: 4,
+        CONNECTED: 5,
+        DISCONNECTED: 6,
+        DISCONNECTING: 7,
+        ATTACHED: 8
+    },
 
 
 
-        connect: function(server, jid, password, callback) {
 
-            var connection = new Strophe.Connection('http://'+server+'/http-bind');
+    connect: function(server, jid, password, callback) {
 
-            connection.rawInput = function (data) {
-                // console.log('RECV: ' + data);
-            };
-            connection.rawOutput = function (data) {
-                // console.log('SEND: ' + data);
-            };
+        var connection = new Strophe.Connection('http://'+server+'/http-bind');
 
-            connection.connect(
-                jid,
-                password,
-                function(status) {
-                    if (status == MX.core.Status.ERROR) {
-                        console.log('connection error');
-                    } else if (status == MX.core.Status.CONNECTING) {
-                        console.log('connecting');
-                    } else if (status == MX.core.Status.CONNFAIL) {
-                        console.log('connection fail');
-                    } else if (status == MX.core.Status.AUTHENTICATING) {
-                        console.log('authenticating');
-                    } else if (status == MX.core.Status.AUTHFAIL) {
-                        console.log('authentication fail');
-                    } else if (status == MX.core.Status.CONNECTED) {
-                        console.log('connected');
+        connection.rawInput = function (data) {
+            // console.log('RECV: ' + data);
+        };
+        connection.rawOutput = function (data) {
+            // console.log('SEND: ' + data);
+        };
 
-                        connection.addHandler(
-                            MX.core.onChatMessage,
-                            null,
-                            'message',
-                            'chat'
-                        );
-                        connection.send($pres());
+        connection.connect(
+            jid,
+            password,
+            function(status) {
+                if (status == MX.core.Status.ERROR) {
+                    console.log('connection error');
+                } else if (status == MX.core.Status.CONNECTING) {
+                    console.log('connecting');
+                } else if (status == MX.core.Status.CONNFAIL) {
+                    console.log('connection fail');
+                } else if (status == MX.core.Status.AUTHENTICATING) {
+                    console.log('authenticating');
+                } else if (status == MX.core.Status.AUTHFAIL) {
+                    console.log('authentication fail');
+                } else if (status == MX.core.Status.CONNECTED) {
+                    console.log('connected');
 
-                    } else if (status == MX.core.Status.DISCONNECTED) {
-                        console.log('disconnected');
-                    } else if (status == MX.core.Status.DISCONNECTING) {
-                        console.log('disconnecting');
-                    } else if (status == MX.core.Status.ATTACHED) {
-                        console.log('attached');
-                    }
-                    if (callback) {
-                        callback(status);
-                    }
+                    connection.addHandler(
+                        MX.core.onChatMessage,
+                        null,
+                        'message',
+                        'chat'
+                    );
+                    connection.send($pres());
+
+                } else if (status == MX.core.Status.DISCONNECTED) {
+                    console.log('disconnected');
+                } else if (status == MX.core.Status.DISCONNECTING) {
+                    console.log('disconnecting');
+                } else if (status == MX.core.Status.ATTACHED) {
+                    console.log('attached');
                 }
-            );
-
-            MX.connection = connection;
-        },
-
-
-        onChatMessage : function(message){
-            console.log('private message:',message);
-        },
-
-
-        disconnect : function(reason) {
-
-            if (MX.connection) {
-                if (MX.connection.connected) {
-                    MX.connection.send($pres({
-                        type : 'unavailable'
-                    }));
-                    MX.connection.disconnect(reason);
-                };
+                if (callback) {
+                    callback(status);
+                }
             }
+        );
 
-        },
-
-
-
-
-        send: function(elem) {
-            MX.connection.send(elem);
-        },
+        MX.connection = connection;
+    },
 
 
+    onChatMessage : function(message){
+        console.log('private message:',message);
+    },
 
 
-        sendIQ: function(elem, callback, errback) {
-            if (MX.connection) {
-                MX.connection.sendIQ(elem, callback, errback, 3000);
-            } else {
-                errback('MX.conneciton not defined');
-            }
-        },
+    disconnect : function(reason) {
 
-
-
-
-
-        discoverService: function(service, callback, errback, version) {
-
-            var iq = $iq({
-                to: jQuery.jStorage.get('settings').coordinator,
-                type: 'get'
-            })
-            .c('serviceDiscovery', {
-                xmlns: MX.NS.COORDINATOR
-            });
-
-            if (service && service !== null) {
-                iq.c('serviceNamespace').t(service);
+        if (MX.connection) {
+            if (MX.connection.connected) {
+                MX.connection.send($pres({
+                    type : 'unavailable'
+                }));
+                MX.connection.disconnect(reason);
             };
+        }
 
-            if (version && version !== null) {
-                iq.up().c('serviceVersion').t(version);
-            };
-
-            MX.core.sendIQ(iq, callback, errback);
-        },
+    },
 
 
 
 
-    };
-
-    MX.extend('core', core);
-
-})();
+    send: function(elem) {
+        MX.connection.send(elem);
+    },
 
 
-$(document).on('ready', function(){
 
-    if ( !jQuery.jStorage.index().length ){
 
-        $.getJSON('settings.json', function(data){
-            jQuery.jStorage.set('settings', data);
-            console.log('imported settings');
+    sendIQ: function(elem, callback, errback) {
+        if (MX.connection) {
+            MX.connection.sendIQ(elem, callback, errback, 3000);
+        } else {
+            errback('MX.conneciton not defined');
+        }
+    },
+
+
+
+
+
+    discoverService: function(service, callback, errback, version) {
+
+        var iq = $iq({
+            to: jQuery.jStorage.get('settings').coordinator,
+            type: 'get'
+        })
+        .c('serviceDiscovery', {
+            xmlns: MX.NS.COORDINATOR
         });
 
-    }
-});
+        if (service && service !== null) {
+            iq.c('serviceNamespace').t(service);
+        };
+
+        if (version && version !== null) {
+            iq.up().c('serviceVersion').t(version);
+        };
+
+        MX.core.sendIQ(iq, callback, errback);
+    },
 
 
-$(window).unload(function() {
 
-    MX.core.disconnect('Browser Window Closed');
 
-});
+};
+
+MX.extend('core', core);
--- MXJS/mx.ninecards.js
+++ MXJS/mx.ninecards.js
@@ -1,74 +1,70 @@
-(function() {
-
-    var ninecards = {
+var ninecards = {
 
 
-        init: function() {
-            MX.addNamespace('SERVICE', MX.NS.URL + '#services/MobilisNineCardsService');
-            MX.addNamespace('CONFIGUREGAME', 'mobilisninecards:iq:configuregame');
-            MX.addNamespace('JOINGAME', 'mobilisninecards:iq:joingame');
-        },
+    init: function() {
+        MX.addNamespace('SERVICE', MX.NS.URL + '#services/MobilisNineCardsService');
+        MX.addNamespace('CONFIGUREGAME', 'mobilisninecards:iq:configuregame');
+        MX.addNamespace('JOINGAME', 'mobilisninecards:iq:joingame');
+    },
 
 
-        createServiceInstance: function (name, resultcallback, errorcallback) {
-            var settings = jQuery.jStorage.get('settings');
-            var customIq = $iq({
-                to: settings.coordinator,
-                type: 'set'                
-            })
-            .c('createNewServiceInstance', {xmlns: MX.NS.COORDINATOR} )
-            .c('serviceNamespace').t(MX.NS.SERVICE).up()
-            .c('serviceName').t(name);
+    createServiceInstance: function (name, resultcallback, errorcallback) {
+        var settings = jQuery.jStorage.get('settings');
+        var customIq = $iq({
+            to: settings.coordinator,
+            type: 'set'                
+        })
+        .c('createNewServiceInstance', {xmlns: MX.NS.COORDINATOR} )
+        .c('serviceNamespace').t(MX.NS.SERVICE).up()
+        .c('serviceName').t(name);
 
-            MX.core.sendIQ(customIq, resultcallback, errorcallback);
-        },
+        MX.core.sendIQ(customIq, resultcallback, errorcallback);
+    },
 
 
-        ConfigureGame: function(gameJID, GameName, MaxPlayers, NumberOfRounds, resultcallback, errorcallback) {
-            if (!resultcallback) 
-                resultcallback = MX.core.defaultcallback; 
-            if (!errorcallback) 
-                errorcallback = MX.core.defaulterrorback;
+    ConfigureGame: function(gameJID, GameName, MaxPlayers, NumberOfRounds, resultcallback, errorcallback) {
+        if (!resultcallback) 
+            resultcallback = MX.core.defaultcallback; 
+        if (!errorcallback) 
+            errorcallback = MX.core.defaulterrorback;
 
-            var customIq = $iq({
-                to: gameJID,
+        var customIq = $iq({
+            to: gameJID,
+            type: 'set'
+        })
+        .c('ConfigureGameRequest', {xmlns : MX.NS.CONFIGUREGAME})
+        .c('gamename').t(GameName).up()
+        .c('players').t(MaxPlayers).up()
+        .c('rounds').t(NumberOfRounds).up();
+
+        MX.core.sendIQ(customIq, resultcallback, errorcallback);
+    }, 
+
+
+
+
+    joinGame: function(gameJid, resultcallback, errorcallback) {
+        if (!resultcallback) 
+            resultcallback = MX.core.defaultcallback; 
+        if (!errorcallback) 
+            errorcallback = MX.core.defaulterrorback;
+             
+        if (gameJid){ 
+            MX.ninecards.gameJID = gameJid;
+            var customiq = $iq({
+                to: gameJid,
                 type: 'set'
             })
-            .c('ConfigureGameRequest', {xmlns : MX.NS.CONFIGUREGAME})
-            .c('gamename').t(GameName).up()
-            .c('players').t(MaxPlayers).up()
-            .c('rounds').t(NumberOfRounds).up();
-
-            MX.core.sendIQ(customIq, resultcallback, errorcallback);
-        }, 
-
-
-
-
-        joinGame: function(gameJid, resultcallback, errorcallback) {
-            if (!resultcallback) 
-                resultcallback = MX.core.defaultcallback; 
-            if (!errorcallback) 
-                errorcallback = MX.core.defaulterrorback;
-                 
-            if (gameJid){ 
-                MX.ninecards.gameJID = gameJid;
-                var customiq = $iq({
-                    to: gameJid,
-                    type: 'set'
-                })
-                .c('JoinGameRequest' , {xmlns : MX.NS.JOINGAME}).up();
-            } else {
-                errorcallback(null, 'Game JID not defined');
-            }
-
-            MX.core.sendIQ(customiq, resultcallback, errorcallback);
+            .c('JoinGameRequest' , {xmlns : MX.NS.JOINGAME}).up();
+        } else {
+            errorcallback(null, 'Game JID not defined');
         }
 
-
-
+        MX.core.sendIQ(customiq, resultcallback, errorcallback);
     }
 
-    MX.extend('ninecards', ninecards);
 
-})();
+
+}
+
+MX.extend('ninecards', ninecards);
